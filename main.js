454cb0223839e629caf139b09b0c01c62362f297
Kyle Stratton <kdstratton@gmail.com>
2019-03-17 14:25:12 -0400

    Added hiring unskilled workers

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 var tick_time = 1000;
 function draw_resource_bar(state) {
-    var money_rate = state.timers.base.amnt /
+    var money_rate = (1.0 * state.timers.base.amnt) /
         state.timers.base.limit;
-    state.ui.res_bar.innerHTML =
-        "Money: $" + state.res.money + " ($" + money_rate + "/s)";
+    var wages = (state.timers.wages.amnt);
+    var pm = money_rate;
+    var phr = 0 - wages;
+    document.getElementById("resource-bar").innerHTML =
+        "Money: $" + state.res.money + " ($" + pm + "/min)"
+            + "($" + phr + "/hr)";
+}
+function draw_worker_area(state) {
+    document.getElementById("unskilled-text").innerHTML =
+        "Unskilled Workers: " + state.res.unsk_w + " ($"
+            + state.prices.unsk_w + "/hr)";
+}
+function draw(state) {
+    draw_resource_bar(state);
+    draw_worker_area(state);
 }
 function tick(state) {
     state.ticks += 1;
     if (state.ticks - state.timers.base.prev >
         state.timers.base.limit) {
         state.res.money += state.timers.base.amnt;
+        state.timers.base.prev = state.ticks;
     }
-    draw_resource_bar(state);
-}
-// game entry point
-function init() {
-    var gstate = {
-        time_prev: 0,
-        time: (new Date()).getTime(),
-        ticks: 0,
-        ui: {
-            res_bar: document.getElementById("resource-bar")
-        },
-        timers: {
-            base: {
-                limit: 1,
-                prev: 0,
-                amnt: 1
-            }
-        },
-        res: {
-            money: 0
-        }
-    };
-    update(gstate);
+    if (state.ticks - state.timers.wages.prev >
+        state.timers.wages.limit) {
+        state.res.money -= state.timers.wages.amnt;
+        state.timers.wages.prev = state.ticks;
+    }
+    draw(state);
 }
 // frame update
 function update(state) {
@@ -44,4 +41,38 @@ function update(state) {
     }
     requestAnimationFrame((function () { return update(state); }));
 }
-init();
+var gstate = {
+    time_prev: 0,
+    time: (new Date()).getTime(),
+    ticks: 0,
+    timers: {
+        base: {
+            limit: 1,
+            prev: 0,
+            amnt: 1
+        },
+        wages: {
+            limit: 60,
+            prev: 0,
+            amnt: 0
+        }
+    },
+    res: {
+        money: 0,
+        unsk_w: 0
+    },
+    prices: {
+        unsk_w: 8
+    }
+};
+update(gstate);
+function unskilled_hire() {
+    gstate.timers.wages.amnt += 8;
+    gstate.res.unsk_w += 1;
+    draw(gstate);
+}
+function unskilled_fire() {
+    gstate.timers.wages.amnt -= 8;
+    gstate.res.unsk_w -= 1;
+    draw(gstate);
+}
--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-interface UserInterface {
-	res_bar: HTMLElement;
-}
-
 interface Resources {
 	money: number;
+	unsk_w: number;
+}
+
+interface Prices {
+	unsk_w: number;
 }
 
 interface Timer {
@@ -14,24 +15,40 @@ interface Timer {
 
 interface Timers {
 	base: Timer;
+	wages: Timer;
 }
 
 interface GameState {
 	time_prev: number;
 	time: number;
 	ticks: number;
-	ui: UserInterface;
 	timers: Timers;
 	res: Resources;
+	prices: Prices;
 }
 
 const tick_time = 1000;
 
 function draw_resource_bar(state: GameState) {
-	const money_rate = state.timers.base.amnt /
+	const money_rate = (1.0 * state.timers.base.amnt) /
 		state.timers.base.limit;
-	state.ui.res_bar.innerHTML = 
-		"Money: $" + state.res.money + " ($" + money_rate + "/s)";
+	const wages = (state.timers.wages.amnt)
+	const pm = money_rate;
+	const phr = 0 - wages;
+	document.getElementById("resource-bar").innerHTML =
+		"Money: $" + state.res.money + " ($" + pm + "/min)"
+		+ "($" + phr + "/hr)";
+}
+
+function draw_worker_area(state: GameState) {
+	document.getElementById("unskilled-text").innerHTML =
+		"Unskilled Workers: " + state.res.unsk_w + " ($"
+		+ state.prices.unsk_w + "/hr)";
+}
+
+function draw(state: GameState) {
+	draw_resource_bar(state);
+	draw_worker_area(state);
 }
 
 function tick(state: GameState) {
@@ -39,31 +56,14 @@ function tick(state: GameState) {
 	if (state.ticks - state.timers.base.prev > 
 		 state.timers.base.limit) {
 		state.res.money += state.timers.base.amnt;
+		state.timers.base.prev = state.ticks;
 	}
-	draw_resource_bar(state);
-}
-
-// game entry point
-function init() {
-	let gstate = {
-		time_prev: 0,
-		time:(new Date()).getTime(),
-		ticks: 0,
-		ui: {
-			res_bar: document.getElementById("resource-bar")
-		},
-		timers: {
-			base: {
-				limit: 1,
-				prev: 0,
-				amnt: 1
-			}
-		},
-		res: {
-			money: 0
-		}
+	if (state.ticks - state.timers.wages.prev > 
+		 state.timers.wages.limit) {
+		state.res.money -= state.timers.wages.amnt;
+		state.timers.wages.prev = state.ticks;
 	}
-	update(gstate);
+	draw(state);
 }
 
 // frame update
@@ -76,4 +76,42 @@ function update(state: GameState) {
 	requestAnimationFrame((()=>update(state)));
 }
 
-init();
+let gstate = {
+	time_prev: 0,
+	time:(new Date()).getTime(),
+	ticks: 0,
+	timers: {
+		base: {
+			limit: 1,
+			prev: 0,
+			amnt: 1
+		},
+		wages: {
+			limit: 60,
+			prev: 0,
+			amnt: 0
+		}
+	},
+	res: {
+		money: 0,
+		unsk_w: 0
+	},
+	prices: {
+		unsk_w: 8
+	}
+}
+update(gstate);
+
+function unskilled_hire() {
+	gstate.timers.wages.amnt += 8;
+	gstate.res.unsk_w += 1;
+	draw(gstate);
+}
+
+function unskilled_fire() {
+	gstate.timers.wages.amnt -= 8;
+	gstate.res.unsk_w -= 1;
+	draw(gstate);
+}
+
+
