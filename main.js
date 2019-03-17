b7b7a53dfa92a866539509ebfea2ec0922c8fbeb
Kyle Stratton <kdstratton@gmail.com>
2019-03-16 22:19:22 -0400

    Added game loop

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+var tick_time = 1000;
 function hello(person) {
     return "HELLO " + person;
 }
 var user = "KYLE";
 document.title = hello(user);
+function tick(state) {
+    state.val += 1;
+    console.log(state);
+}
+var gstate = { prev_time: 0, time: (new Date()).getTime(), val: 0 };
+function update() {
+    gstate.time = (new Date()).getTime();
+    if (gstate.time - gstate.prev_time > tick_time) {
+        tick(gstate);
+        gstate.prev_time = gstate.time;
+    }
+    requestAnimationFrame(update);
+}
+update();
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+interface GameState {
+	prev_time: number;
+	time: number;
+	val: number;
+}
+
+const tick_time = 1000;
+
 function hello(person: string) {
 	return "HELLO " + person;
 }
@@ -5,3 +13,21 @@ function hello(person: string) {
 const user = "KYLE";
 
 document.title = hello(user);
+
+function tick(state: GameState) {
+	state.val += 1;
+	console.log(state);
+}
+
+let gstate = {prev_time: 0, time:(new Date()).getTime(), val:0}
+
+function update() {
+	gstate.time = (new Date()).getTime();
+	if (gstate.time - gstate.prev_time > tick_time) {
+		tick(gstate);
+		gstate.prev_time = gstate.time;
+	}
+	requestAnimationFrame(update);
+}
+
+update();
