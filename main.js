b1e9046f49e30ef3b3a1b666d86cf251fb4c2cea
Kyle Stratton <kdstratton@gmail.com>
2019-03-17 15:22:21 -0400

    Added upgrades

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var tick_time = 1000;
+/* Draw functions */
 function draw_resource_bar(state) {
     var money_rate = (1.0 * state.timers.base.amnt) /
         state.timers.base.limit;
@@ -7,32 +8,40 @@ function draw_resource_bar(state) {
     var phr = 0 - wages;
     document.getElementById("resource-bar").innerHTML =
         "Money: $" + state.res.money + " ($" + pm + "/min)"
-            + "($" + phr + "/hr)";
+            + "($" + phr + "/hr)<br>Workers: " + state.res.workers;
 }
 function draw_worker_area(state) {
     document.getElementById("unskilled-text").innerHTML =
         "Unskilled Workers: " + state.res.unsk_w + " ($"
             + state.prices.unsk_w + "/hr)";
+    document.getElementById("manager-text").innerHTML =
+        "Managers: " + state.res.manag + " ($"
+            + state.prices.manag + "/hr)";
 }
 function draw(state) {
     draw_resource_bar(state);
     draw_worker_area(state);
 }
+function unhide(state) {
+    if (!state.upgrades.basic1 && state.res.money >= 10) {
+        document.getElementById("basic-res-1").style.display = "inline";
+    }
+}
 function tick(state) {
     state.ticks += 1;
-    if (state.ticks - state.timers.base.prev >
+    if (state.ticks - state.timers.base.prev >=
         state.timers.base.limit) {
         state.res.money += state.timers.base.amnt;
         state.timers.base.prev = state.ticks;
     }
-    if (state.ticks - state.timers.wages.prev >
+    if (state.ticks - state.timers.wages.prev >=
         state.timers.wages.limit) {
         state.res.money -= state.timers.wages.amnt;
         state.timers.wages.prev = state.ticks;
     }
     draw(state);
+    unhide(state);
 }
-// frame update
 function update(state) {
     state.time = (new Date()).getTime();
     if (state.time - state.time_prev > tick_time) {
@@ -59,20 +68,47 @@ var gstate = {
     },
     res: {
         money: 0,
-        unsk_w: 0
+        unsk_w: 0,
+        manag: 0,
+        workers: 0
     },
     prices: {
-        unsk_w: 8
+        unsk_w: 9,
+        manag: 15
+    },
+    upgrades: {
+        basic1: false
     }
 };
 update(gstate);
+/* onclick functions */
 function unskilled_hire() {
-    gstate.timers.wages.amnt += 8;
+    gstate.timers.wages.amnt += gstate.prices.unsk_w;
     gstate.res.unsk_w += 1;
+    gstate.res.workers += 1;
     draw(gstate);
 }
 function unskilled_fire() {
-    gstate.timers.wages.amnt -= 8;
+    gstate.timers.wages.amnt -= gstate.prices.unsk_w;
     gstate.res.unsk_w -= 1;
+    gstate.res.workers -= 1;
+    draw(gstate);
+}
+function basic_research_1() {
+    document.getElementById("basic-res-1").style.display = "none";
+    gstate.upgrades.basic1 = true;
+    document.getElementById("manager-tab").style.display = "inline";
+    draw(gstate);
+}
+function manager_hire() {
+    gstate.timers.wages.amnt += gstate.prices.manag;
+    gstate.res.manag += 1;
+    gstate.res.workers += 1;
+    draw(gstate);
+}
+function manager_fire() {
+    gstate.timers.wages.amnt -= gstate.prices.unsk_w;
+    gstate.res.manag -= 1;
+    gstate.res.workers -= 1;
     draw(gstate);
 }
--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 interface Resources {
 	money: number;
 	unsk_w: number;
+	manag: number;
+	workers: number;
 }
 
 interface Prices {
 	unsk_w: number;
+	manag: number;
 }
 
 interface Timer {
@@ -18,6 +21,10 @@ interface Timers {
 	wages: Timer;
 }
 
+interface Upgrades {
+	basic1: Boolean,
+}
+
 interface GameState {
 	time_prev: number;
 	time: number;
@@ -25,9 +32,12 @@ interface GameState {
 	timers: Timers;
 	res: Resources;
 	prices: Prices;
+	upgrades: Upgrades
 }
 
 const tick_time = 1000;
+
+/* Draw functions */
 
 function draw_resource_bar(state: GameState) {
 	const money_rate = (1.0 * state.timers.base.amnt) /
@@ -37,13 +47,16 @@ function draw_resource_bar(state: GameState) {
 	const phr = 0 - wages;
 	document.getElementById("resource-bar").innerHTML =
 		"Money: $" + state.res.money + " ($" + pm + "/min)"
-		+ "($" + phr + "/hr)";
+		+ "($" + phr + "/hr)<br>Workers: " + state.res.workers;
 }
 
 function draw_worker_area(state: GameState) {
 	document.getElementById("unskilled-text").innerHTML =
 		"Unskilled Workers: " + state.res.unsk_w + " ($"
 		+ state.prices.unsk_w + "/hr)";
+	document.getElementById("manager-text").innerHTML =
+		"Managers: " + state.res.manag + " ($"
+		+ state.prices.manag + "/hr)";
 }
 
 function draw(state: GameState) {
@@ -51,22 +64,28 @@ function draw(state: GameState) {
 	draw_worker_area(state);
 }
 
+function unhide(state: GameState) {
+	if (!state.upgrades.basic1 && state.res.money >= 10) {
+		document.getElementById("basic-res-1").style.display = "inline";
+	}
+}
+
 function tick(state: GameState) {
 	state.ticks += 1;
-	if (state.ticks - state.timers.base.prev > 
+	if (state.ticks - state.timers.base.prev >= 
 		 state.timers.base.limit) {
 		state.res.money += state.timers.base.amnt;
 		state.timers.base.prev = state.ticks;
 	}
-	if (state.ticks - state.timers.wages.prev > 
+	if (state.ticks - state.timers.wages.prev >= 
 		 state.timers.wages.limit) {
 		state.res.money -= state.timers.wages.amnt;
 		state.timers.wages.prev = state.ticks;
 	}
 	draw(state);
+	unhide(state);
 }
 
-// frame update
 function update(state: GameState) {
 	state.time = (new Date()).getTime();
 	if (state.time - state.time_prev > tick_time) {
@@ -94,24 +113,54 @@ let gstate = {
 	},
 	res: {
 		money: 0,
-		unsk_w: 0
+		unsk_w: 0,
+		manag: 0,
+		workers: 0
+		
 	},
 	prices: {
-		unsk_w: 8
+		unsk_w: 9,
+		manag: 15
+	},
+	upgrades: {
+		basic1: false
 	}
 }
 update(gstate);
 
+/* onclick functions */
+
 function unskilled_hire() {
-	gstate.timers.wages.amnt += 8;
+	gstate.timers.wages.amnt += gstate.prices.unsk_w;
 	gstate.res.unsk_w += 1;
+	gstate.res.workers += 1;
 	draw(gstate);
 }
 
 function unskilled_fire() {
-	gstate.timers.wages.amnt -= 8;
+	gstate.timers.wages.amnt -= gstate.prices.unsk_w;
 	gstate.res.unsk_w -= 1;
+	gstate.res.workers -= 1;
 	draw(gstate);
 }
 
+function basic_research_1() {
+	document.getElementById("basic-res-1").style.display = "none";
+	gstate.upgrades.basic1 = true;
+	document.getElementById("manager-tab").style.display = "inline";
+	draw(gstate);
+}
 
+function manager_hire() {
+	gstate.timers.wages.amnt += gstate.prices.manag;
+	gstate.res.manag += 1;
+	gstate.res.workers += 1;
+	draw(gstate);
+}
+
+function manager_fire() {
+	gstate.timers.wages.amnt -= gstate.prices.unsk_w;
+	gstate.res.manag -= 1;
+	gstate.res.workers -= 1;
+	draw(gstate);
+}
